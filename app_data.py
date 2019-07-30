4dda78c6bb3b6fe6b5c4c491fb9746783cf98f5d
Samuel <samuelsoares177778@gmail.com>
2019-07-30 19:15:55 -0300

    add data and opened in app-data

--- app_data.py
+++ app_data.py
@@ -1,2 +1,17 @@
 import matplotlib.pyplot as plt
 
+dados = open("DATA/populacao-brasileira.csv").readlines()
+
+eixo_x = []
+eixo_y = []
+
+for i in range(len(dados)):
+    if i != 0:
+        linha = dados[i].split(";")
+        eixo_x.append(int(linha[0]))
+        eixo_y.append(int(linha[1]))
+
+plt.plot(eixo_x,eixo_y)
+plt.bar(eixo_x,eixo_y)
+
+plt.show()
